7a16317d8382dc86c4b6942abc73f99e6d0ffac0
henrique.kops <henrique.kops@edu.pucrs.br>
2022-06-02 07:18:58 -0300

    start diffie-hellman code and test

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+# built-in dependencies
+from sys import (
+	argv, 
+	exit
+)
+from random import randint
+from math import pow
+
+# project dependencies
+from src.diffie import DiffieHellman
+
 __author__ = "Henrique Kops"
 
 
+HELP = "python3 main.py <g:int> <p:int>"
+
 if __name__ == "__main__":
-    print("Hello World")
+
+	if len(argv) !=3:
+		print(HELP)
+		exit(0)
+	
+	p = argv[1]
+	g = argv[2]
+
+	a = randint(pow(10,30),pow(10,31))
+
+	d = DiffieHellman(a)
+
+	print(a)
---
+++ test/diffie_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+__author__ = "Henrique Kops"
+
+import unittest
+
+
+class TestDiffieHellman(unittest.TestCase):
+
+	def test_encrypt(self):
+		pass
+
+	def test_decrypt(self):
+		pass
+
+
+if __name__ == "__main__":
+	unittest.main()
