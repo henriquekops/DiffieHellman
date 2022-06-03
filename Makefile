231c64f7f607f1fe1eecebd0935e430a4b0cf10a
Henrique Reis Kops <henrique.kops@zenvia.com>
2022-06-03 09:05:16 -0300

    improve test

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	python src/main.py
+
+test:
+	python -m unittest src/test/diffie_test.py
--- src/test/diffie_test.py
+++ src/test/diffie_test.py
@@ -7,25 +7,25 @@ import unittest
 
 # project dependencies
 from src.main.diffie import DiffieHellman
-from src.test.utils import (
-	a,
-	p, 
-	g, 	
-	A,
-	B,
-	V
-)
 
 
 class TestDiffieHellman(unittest.TestCase):
 
+	def setUp(self):
+		self.a = 123456789012345678901234567890123456789
+		self.p = 1041607122029938459843911326429539139964006065005940226363139
+		self.g = 10
+		self.A = 372039332014874382386471904578699975832029010553081171186923
+		self.B = 785637473337331550103697723492144500354289194897294297600528
+		self.V = 531549124896866458964697691745629549010303164318920061744393
+
 	def test_encrypt(self):
-		d = DiffieHellman(a)
-		assert d.run(g, p), A
+		d = DiffieHellman(self.a)
+		assert d.run(self.g, self.p), self.A
 
 	def test_decrypt(self):
-		d = DiffieHellman(a)
-		assert d.run(B, p), V
+		d = DiffieHellman(self.a)
+		assert d.run(self.B, self.p), self.V
 
 
 if __name__ == "__main__":
